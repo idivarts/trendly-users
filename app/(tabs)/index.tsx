bd46b9a34ced20bf1e118094849eb377bc7d8ac9
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-07-22 00:30:53 +0530

    Created counter application

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { useContext } from 'react';
+import CounterContext from '@/contexts/CounterContext';
 
 export default function TabOneScreen() {
+  const { counter, setCounter } = useContext(CounterContext)
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Button title='Increase Count' onPress={() => { setCounter(counter + 1) }}></Button>
     </View>
   );
 }
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { useContext } from 'react';
+import CounterContext from '@/contexts/CounterContext';
 
 export default function TabTwoScreen() {
+  const { counter, setCounter } = useContext(CounterContext)
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Current Count: {counter}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <Button title='Reset Counter' onPress={() => setCounter(0)} />
     </View>
   );
 }
