fb0d26594a3236df937d8b06b40f7283b86990ce
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-07-22 20:53:12 +0530

    Added a complex part2 for the question

--- components/coding-test/CodingQ2.tsx
+++ components/coding-test/CodingQ2.tsx
@@ -9,6 +9,8 @@ const CodingQ2 = () => {
             <Text>2. No events can run in parallel</Text>
             <Text>3. Schedule the events so the above two conditions are sufficed</Text>
             <Text>4. Print the start and end time of each events</Text>
+            <Text>--------------------- PART 2 -----------------</Text>
+            <Text>Add restriction so at any given time schedule event that are shorter</Text>
         </View>
     )
 }
