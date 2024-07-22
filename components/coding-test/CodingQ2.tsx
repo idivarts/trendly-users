b301984f37978cd2b54071c8dfb5e32164f591b3
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-07-22 20:51:26 +0530

    Coding question two implemented

--- components/coding-test/CodingQ2.tsx
+++ components/coding-test/CodingQ2.tsx
@@ -4,7 +4,11 @@ import { Text, View } from '../Themed'
 const CodingQ2 = () => {
     return (
         <View>
-            <Text>Coding Round 2</Text>
+            <Text>Coding Round 2 - Scheduling events in order</Text>
+            <Text>1. Events should be scheduled only after there preferred time</Text>
+            <Text>2. No events can run in parallel</Text>
+            <Text>3. Schedule the events so the above two conditions are sufficed</Text>
+            <Text>4. Print the start and end time of each events</Text>
         </View>
     )
 }
