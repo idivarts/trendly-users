9a4c77b03290c374ebdb00b3255c23b041edaf87
Manan Sharma <112090668+Manan-Sharma-5@users.noreply.github.com>
2024-10-02 13:19:44 +0530

    pushed changes

--- utils/auth.ts
+++ utils/auth.ts
@@ -3,16 +3,12 @@ import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { FirebaseApp } from "./firebase";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-let AuthApp: Auth;
-
-if (!getAuth(FirebaseApp)._isInitialized) {
-  AuthApp = initializeAuth(FirebaseApp, {
-    persistence: getReactNativePersistence(AsyncStorage),
-  });
-  console.log("AuthApp initialized");
-} else {
-  AuthApp = getAuth(FirebaseApp);
-  console.log("AuthApp reused");
+if (!FirebaseApp) {
+  throw new Error("Firebase app is not initialized");
 }
+
+const AuthApp = initializeAuth(FirebaseApp, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
 
 export { AuthApp };
