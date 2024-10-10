26b232c5f840d6426f0cf4d3acd76b0fb92d31d1
Devender Singh Rathore <59788555+dev-rathore@users.noreply.github.com>
2024-10-10 12:28:44 +0530

    Push notifications on web (#44)
    * schema changes

--- types/User.ts
+++ types/User.ts
@@ -1,13 +1,5 @@
 import { IUsers } from "@/shared-libs/firestore/trendly-pro/models/users";
 
-export type PushNotificationToken = {
-  ios?: string;
-  android?: string;
-  web?: string;
-};
-
 export interface User extends IUsers {
   id: string;
-  profileImage: string;
-  pushNotificationToken: PushNotificationToken;
 }
