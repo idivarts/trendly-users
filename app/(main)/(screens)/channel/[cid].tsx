6016375e3f4c96aad2e576e98300a4091810aeb1
dev-rathore <rathoredvd3001@gmail.com>
2024-11-06 23:18:24 +0530

    name fix

--- app/(main)/(screens)/channel/[cid].tsx
+++ app/(main)/(screens)/channel/[cid].tsx
@@ -1,13 +1,13 @@
-import ChannelListNative from "@/components/channel/channel";
-import ChannelListWeb from "@/components/channel/channel.web";
+import ChannelNative from "@/components/channel/channel";
+import ChannelWeb from "@/components/channel/channel.web";
 import { Platform } from "react-native";
 
 const ChannelScreen = () => {
   if (Platform.OS === 'web') {
-    return <ChannelListWeb />
+    return <ChannelWeb />
   }
   return (
-    <ChannelListNative />
+    <ChannelNative />
   );
 }
 
