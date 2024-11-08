532cc5d8a7b7be0be5d67ade0fdb688d25c2a373
Rahul Sinha <rahul@idiv.in>
2024-11-08 21:43:39 +0530

    Made changes and added build config (#56)

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,17 +1,12 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { firebaseConfig } from "../firebase-config";
 
-const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID!,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID!,
-};
+
+// const firebaseConfig = firebaseConfi
 
 const FirebaseApp = !getApps().length
   ? initializeApp(firebaseConfig)
   : getApp();
 
-export { firebaseConfig, FirebaseApp };
+export { FirebaseApp, firebaseConfig };
+
