9b67769f99b93e6b85e1f9de23e1abb2d8d625a5
dev-rathore <rathoredvd3001@gmail.com>
2024-11-26 23:14:11 +0530

    uploadImage change to uploadImageBytes

--- contexts/firebase-storage-context.provider.tsx
+++ contexts/firebase-storage-context.provider.tsx
@@ -8,11 +8,13 @@ import {
   ref,
   uploadString,
   getDownloadURL,
+  uploadBytes,
 } from "firebase/storage";
 import { StorageApp } from "@/utils/firebase-storage";
 
 interface FirebaseStorageContextProps {
   uploadImage: (image: string, path: string) => Promise<string>;
+  uploadImageBytes: (blob: Blob, path: string) => Promise<string>;
 }
 
 const FirebaseStorageContext = createContext<FirebaseStorageContextProps>(null!);
@@ -25,9 +27,21 @@ export const FirebaseStorageContextProvider: React.FC<PropsWithChildren> = ({
   const uploadImage = async (
     image: string,
     path: string,
+    format: "base64" | "base64url" | "data_url" | "raw" = "data_url",
   ): Promise<string> => {
     const storageRef = ref(StorageApp, path);
-    await uploadString(storageRef, image, "data_url");
+    await uploadString(storageRef, image, format);
+
+    const downloadURL = await getDownloadURL(storageRef);
+    return downloadURL;
+  }
+
+  const uploadImageBytes = async (
+    blob: Blob,
+    path: string,
+  ): Promise<string> => {
+    const storageRef = ref(StorageApp, path);
+    await uploadBytes(storageRef, blob);
 
     const downloadURL = await getDownloadURL(storageRef);
     return downloadURL;
@@ -37,6 +51,7 @@ export const FirebaseStorageContextProvider: React.FC<PropsWithChildren> = ({
     <FirebaseStorageContext.Provider
       value={{
         uploadImage,
+        uploadImageBytes,
       }}
     >
       {children}
