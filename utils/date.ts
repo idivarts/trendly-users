a8f12252c2be6bb46cc89fbc2ed4db3e236be035
dev-rathore <rathoredvd3001@gmail.com>
2025-01-13 20:24:27 +0530

    date format

--- utils/date.ts
+++ utils/date.ts
@@ -1,53 +1,52 @@
 import { DateArg, formatDistanceToNow } from "date-fns";
 
 // Examples:
-// For seconds: "sec ago"
-// For minutes: "mins ago"
-// For hours: "hrs ago"
-// For days: "d ago"
-// For weeks: "w ago"
-// For months: "m ago"
-// For years: "yrs ago"
+// For seconds: 1 second ago
+// For minutes: 1m ago
+// For hours: 1h ago
+// For days: 1d ago
+// For months: 1mon ago
+// For years: 1y ago
 export const formatTimeToNow = (date: DateArg<Date>): string => {
   return formatDistanceToNow(date, {
     addSuffix: true,
     locale: {
       formatDistance: (token, count, options) => {
         if (token === "lessThanXSeconds") {
-          return `${count} sec ago`;
+          return `${count} second ago`;
         }
         if (token === "xSeconds") {
-          return `${count} sec ago`;
+          return `${count} seconds ago`;
         }
         if (token === "halfAMinute") {
-          return `${count} min ago`;
+          return `${count} seconds ago`;
         }
         if (token === "lessThanXMinutes") {
-          return `${count} mins ago`;
+          return `${count}m ago`;
         }
         if (token === "xMinutes") {
-          return `${count} mins ago`;
+          return `${count}m ago`;
         }
         if (token === "aboutXHours") {
-          return `${count} hrs ago`;
+          return `${count}h ago`;
         }
         if (token === "xHours") {
-          return `${count} hrs ago`;
+          return `${count}h ago`;
         }
         if (token === "xDays") {
-          return `${count} d ago`;
+          return `${count}d ago`;
         }
         if (token === "aboutXMonths") {
-          return `${count} m ago`;
+          return `${count}mon ago`;
         }
         if (token === "xMonths") {
-          return `${count} m ago`;
+          return `${count}mon ago`;
         }
         if (token === "aboutXYears") {
-          return `${count} yrs ago`;
+          return `${count}y ago`;
         }
         if (token === "xYears") {
-          return `${count} yrs ago`;
+          return `${count}y ago`;
         }
         return "";
       },
