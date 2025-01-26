9fa748b3e5839792ef93d575711ee78f45058331
Devender Singh Rathore <59788555+dev-rathore@users.noreply.github.com>
2025-01-26 15:12:05 +0530

    fix: queryParams logic (#129)
    * fix: redirection logic with query params (#128)

--- utils/url.ts
+++ utils/url.ts
@@ -13,3 +13,27 @@ export const imageUrl = (image: string | NodeRequire | undefined) => {
     return require("@/assets/images/placeholder-image.jpg");
   }
 };
+
+export const queryParams = (
+  params: Partial<Record<string, string | string[]>>
+) => {
+  let values: string[] = [];
+
+  Object.entries(params).forEach(([key, value]) => {
+    if (key === "not-found") return;
+
+    if (value == null) return;
+
+    if (Array.isArray(value)) {
+      value.forEach((v) => {
+        if (v != null) {
+          values.push(`${encodeURIComponent(key)}=${encodeURIComponent(v)}`);
+        }
+      });
+    } else {
+      values.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`);
+    }
+  });
+
+  return values.length === 0 ? "" : `?${values.join("&")}`;
+};
