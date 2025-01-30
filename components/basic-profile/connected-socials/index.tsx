98acffd192e77448611a165f3869a9e526e568d3
Manan Sharma <112090668+Manan-Sharma-5@users.noreply.github.com>
2025-01-30 23:57:09 +0530

    pushed changes

--- components/basic-profile/connected-socials/index.tsx
+++ components/basic-profile/connected-socials/index.tsx
@@ -1,26 +1,23 @@
 import FacebookLoginButton from "@/components/profile/ConnectWithFacebook";
 import SocialPage from "@/components/profile/SocialPage";
 import { View } from "@/components/theme/Themed";
+import React from "react";
 import { SocialPlatform } from "@/shared-libs/firestore/trendly-pro/constants/social-platform";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FlatList } from "react-native";
-import { collection, getDocs } from "firebase/firestore";
-import { FirestoreDB } from "@/utils/firestore";
-import { AuthApp } from "@/utils/auth";
 import { ActivityIndicator } from "react-native-paper";
 import InstagramLoginButton from "@/components/profile/ConnectWithInstagram";
-import { useAuthContext, useSocialContext } from "@/contexts";
+import { useSocialContext } from "@/contexts";
 
 const ConnectedSocials: React.FC = () => {
   const [loading, setLoading] = useState(false);
-  const { user } = useAuthContext();
   const { socials } = useSocialContext();
 
   return (
     <View
       style={{
         flex: 1,
-        paddingHorizontal: 16,
+        paddingHorizontal: 20,
       }}
     >
       {loading ? (
@@ -48,6 +45,7 @@ const ConnectedSocials: React.FC = () => {
             keyExtractor={(item) => item.id}
             contentContainerStyle={{
               flexGrow: 1,
+              gap: 10,
             }}
           />
           <FacebookLoginButton onFacebookLogin={() => {}} />
