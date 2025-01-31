876aec9cffe8e7e754dd566da291356425437661
dev-rathore <rathoredvd3001@gmail.com>
2025-01-31 16:19:08 +0530

    loading indicator for messages

--- components/channel/channel-list.tsx
+++ components/channel/channel-list.tsx
@@ -73,7 +73,6 @@ const ChannelListNative = () => {
       <ChannelList
         EmptyStateIndicator={EmptyMessageState}
         LoadingErrorIndicator={EmptyMessageState}
-        LoadingIndicator={EmptyMessageState}
         channelRenderFilterFn={customChannelFilterFunction}
         filters={{
           members: { $in: [user?.id as string] },
