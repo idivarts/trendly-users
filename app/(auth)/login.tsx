af3db8f0f85188ee8babfebbd6932728bb0c6f68
Rahul Sinha <rahul@idiv.in>
2025-03-06 18:21:09 +0530

    Removed option to sign up directly

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,12 +1,13 @@
 import Button from "@/components/ui/button";
 import TextInput from "@/components/ui/text-input";
+import { IS_BETA_ENABLED } from "@/constants/App";
 import Colors from "@/constants/Colors";
 import { useAuthContext } from "@/contexts";
 import fnStyles from "@/styles/login.styles";
 import { useTheme } from "@react-navigation/native";
 import { useRouter } from "expo-router";
 import * as React from "react";
-import { Image, KeyboardAvoidingView, Platform, ScrollView, Text, View } from "react-native";
+import { Alert, Image, KeyboardAvoidingView, Linking, Platform, ScrollView, Text, View } from "react-native";
 
 const LoginScreen = () => {
   const [email, setEmail] = React.useState("");
@@ -18,6 +19,24 @@ const LoginScreen = () => {
 
   const handleSignIn = () => {
     signIn(email, password);
+  };
+
+  const handleEmailPress = () => {
+    const email = 'support@idiv.in';
+    const subject = encodeURIComponent('Support Request');
+    const body = encodeURIComponent('Hello, I need assistance with...');
+
+    const mailto = `mailto:${email}?subject=${subject}&body=${body}`;
+
+    Linking.canOpenURL(mailto)
+      .then((supported) => {
+        if (supported) {
+          Linking.openURL(mailto);
+        } else {
+          Alert.alert('Error', 'No email app found on this device.');
+        }
+      })
+      .catch((err) => console.error('An error occurred', err));
   };
 
   return (
@@ -77,7 +96,7 @@ const LoginScreen = () => {
         </ScrollView>
       </KeyboardAvoidingView>
       {/* Sign Up and Forgot Password Links */}
-      <View style={styles.footer}>
+      {IS_BETA_ENABLED ? <View style={styles.footer}>
         <Text style={styles.footerText}>
           Don't have an account?{" "}
           <Text
@@ -91,7 +110,18 @@ const LoginScreen = () => {
             Sign Up
           </Text>
         </Text>
-      </View>
+      </View> : <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Signup with Email is not allowed for regular users?{" "}
+          <Text
+            style={styles.footerLink}
+            onPress={() => { handleEmailPress() }}
+          >
+            Contact Support
+          </Text>
+        </Text>
+      </View>}
+
     </View>
   );
 };
