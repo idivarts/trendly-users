e3971c8f9f8ba167754ee41130b9cfe91bfd6dce
Rahul Sinha <rahul@idiv.in>
2025-05-04 13:22:39 +0530

    Minor update

--- components/channel/channel-list.web.tsx
+++ components/channel/channel-list.web.tsx
@@ -4,15 +4,13 @@ import { useChatContext } from "@/contexts/chat-context.provider";
 import AppLayout from "@/layouts/app-layout";
 import WebMessageWrapper from "@/shared-libs/contexts/web-message-wrapper";
 import { useTheme } from "@react-navigation/native";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { ActivityIndicator } from "react-native-paper";
 import { View } from "../theme/Themed";
 import EmptyMessageState from "./empty-message-state";
 
 const ChannelListWeb = () => {
   const [loading, setLoading] = useState(true)
-  const [iFrameLoaded, setIFrameLoaded] = useState(false)
-  const iFramRef = useRef<HTMLIFrameElement>(null)
   const [token, setToken] = useState("")
   const theme = useTheme()
   const { user } = useAuthContext()
@@ -43,16 +41,10 @@ const ChannelListWeb = () => {
     return <EmptyMessageState />
   }
   return (
-    <WebMessageWrapper iFrameLoaded={iFrameLoaded} iFrameRef={iFramRef} isUser={true}>
-      <iframe
-        ref={iFramRef}
-        src={`/messenger/index.html?user=${user?.id}&user_token=${token}&target_origin=${window.location.origin}&skip_name_image_set=false&no_channel_name_filter=false`}
-        style={{ width: '100%', height: '100%', border: 'none' }}
-        onLoad={() => {
-          setIFrameLoaded(true)
-        }}
-      />
-    </WebMessageWrapper>
+    <WebMessageWrapper isInfluencer={true}
+      influencerManagerid={user?.id || ""}
+      streamToken={token}
+    />
   );
 };
 
