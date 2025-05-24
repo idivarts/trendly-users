5a06fed701f8872fe967284b01b221caa4d5a257
Rahul Sinha <rahul@idiv.in>
2025-05-24 23:45:17 +0530

    renamed some colors

--- constants/Theme.ts
+++ constants/Theme.ts
@@ -1,20 +1,20 @@
+import { Theme } from "@react-navigation/native";
 import { DefaultTheme as PaperDefaultTheme } from "react-native-paper";
 import Colors from "./Colors";
-import { Theme } from "@react-navigation/native";
 
 const CustomPaperTheme = (theme: Theme) => ({
   ...PaperDefaultTheme,
   colors: {
     ...PaperDefaultTheme.colors,
     background: Colors(theme).background,
-    onPrimary: Colors(theme).onPrimary,
+    onPrimary: Colors(theme).white,
     onSecondaryContainer: Colors(theme).onSecondaryContainer,
     onSurface: Colors(theme).onSurface,
     outline: Colors(theme).outline,
     primary: Colors(theme).primary,
     secondaryContainer: Colors(theme).secondaryContainer,
-    surface: Colors(theme).surface,
-    surfaceVariant: Colors(theme).surfaceVariant,
+    surface: Colors(theme).white,
+    surfaceVariant: Colors(theme).white,
     text: Colors(theme).text,
     elevation: {
       level1: Colors(theme).white,
