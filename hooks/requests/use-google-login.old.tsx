46f6376f1e31d20c3c97cebcfdab0da8e9a84608
Rahul Sinha <rahul@idiv.in>
2025-06-23 01:40:11 +0530

    Added feature so as to seamlessly use the firebase for google login

--- hooks/requests/use-google-login.old.tsx
+++ hooks/requests/use-google-login.old.tsx
@@ -1,29 +1,28 @@
 import { useInitialUserData } from "@/constants/User";
 import { useAuthContext } from "@/contexts";
 import { Console } from "@/shared-libs/utils/console";
+import { AuthApp } from "@/shared-libs/utils/firebase/auth";
 import { FirestoreDB } from "@/shared-libs/utils/firebase/firestore";
 import Toaster from "@/shared-uis/components/toaster/Toaster";
+import * as Google from 'expo-auth-session/providers/google';
 import { GoogleAuthProvider, signInWithCredential, UserCredential } from 'firebase/auth';
 import { doc, getDoc, setDoc } from "firebase/firestore";
-
-import { AuthApp } from "@/shared-libs/utils/firebase/auth";
-import {
-    GoogleSignin,
-    isSuccessResponse,
-} from '@react-native-google-signin/google-signin';
-import { useEffect } from "react";
 
 
 export const useGoogleLogin = (setLoading: Function, setError: Function) => {
     const { firebaseSignIn, firebaseSignUp } = useAuthContext();
     const INTIAL_DATA = useInitialUserData()
 
-    useEffect(() => {
-        GoogleSignin.configure({
-            webClientId: "799278694891-mqote8c8hpb4952l2hg9hchkni8js2k5.apps.googleusercontent.com",
-            offlineAccess: true
-        })
-    }, [])
+    // const redirectUri = makeRedirectUri({
+    //     native: 'trendly-creators',
+    // });
+
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        iosClientId: '799278694891-6cubib0gjb4kp81vq5bi8ulu4q7amduv.apps.googleusercontent.com',
+        androidClientId: '799278694891-4boqousbd7vfevm9p8s24n1h5gc9u49o.apps.googleusercontent.com',
+        // webClientId: "799278694891-mqote8c8hpb4952l2hg9hchkni8js2k5.apps.googleusercontent.com",
+        // redirectUri
+    });
 
     const evalResult = async (result: void | UserCredential) => {
         if (!result)
@@ -56,23 +55,15 @@ export const useGoogleLogin = (setLoading: Function, setError: Function) => {
 
     const googleLogin = async () => {
         try {
-            await GoogleSignin.hasPlayServices();
-            if (GoogleSignin.hasPreviousSignIn()) {
-                await GoogleSignin.signOut()
-            }
-            const signInResponse = await GoogleSignin.signIn()
-            if (isSuccessResponse(signInResponse)) {
+            const result = await promptAsync();
+            if (result?.type === 'success' && result.authentication) {
                 setLoading(true);
-                const idToken = signInResponse?.data?.idToken;
-                if (!idToken)
-                    throw new Error("Missing Google ID Token from sign-in response");
-
-                const credential = GoogleAuthProvider.credential(idToken);
+                const credential = GoogleAuthProvider.credential(result.authentication.idToken, result.authentication.accessToken);
                 const firebaseResult = await signInWithCredential(AuthApp, credential);
                 await evalResult(firebaseResult);
-            } else if (signInResponse.type === 'cancelled') {
+            } else {
                 Toaster.error('Google sign-in cancelled or failed');
-                Console.log("Google sign-in cancelled or failed", signInResponse);
+                Console.log("Google sign-in cancelled or failed", result);
                 setError('cancelled');
             }
         } catch (error: any) {
