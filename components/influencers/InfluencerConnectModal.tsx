6aa8081ac6326f54379551eeb38d8c39e9f9559c
Rahul Sinha <rahul@idiv.in>
2025-07-12 11:20:49 +0530

    Added influencer connect modal

---
+++ components/influencers/InfluencerConnectModal.tsx
@@ -0,0 +1,10 @@
+import { View } from '@/shared-uis/components/theme/Themed'
+import React from 'react'
+
+const InfluencerConnectModal = () => {
+    return (
+        <View>InfluencerConnectModal</View>
+    )
+}
+
+export default InfluencerConnectModal
