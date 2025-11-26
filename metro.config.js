06451c8eb264b4b1c21db56ebddaf378772b9efa
Rahul Sinha <rahul@idiv.in>
2025-11-27 00:31:09 +0530

    Minor change

--- metro.config.js
+++ metro.config.js
@@ -21,9 +21,6 @@ module.exports = (() => {
 
   const webAliases = {
     "react-native": "react-native-web",
-    "react-native-webview": "@10play/react-native-web-webview",
-    "react-native/Libraries/Utilities/codegenNativeComponent":
-      "@10play/react-native-web-webview/shim",
   };
 
   config.resolver.resolveRequest = (
