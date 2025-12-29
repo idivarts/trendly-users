483762fe9c4a07407c6f17a54b7ec6ddf6cd3b66
Rahul Sinha <rahul@idiv.in>
2025-12-29 12:11:56 +0530

    Added options for insights

--- constants/App.ts
+++ constants/App.ts
@@ -7,6 +7,8 @@ export const APP_SCHEME = "trendly-creators";
 export const APP_STORE_URL = CREATORS_APPSTORE_URL;
 export const PLAY_STORE_URL = CREATORS_PLAYSTORE_URL;
 
-export const IS_INSTA_ENABLED = (process.env.EXPO_PUBLIC_APP_STAGE == "dev");
+export const IS_INSTA_ENABLED = true;
+
+export const IS_INSIGHT_ENABLED = false; //(process.env.EXPO_PUBLIC_APP_STAGE == "dev")
 
 export const IS_BETA_ENABLED = Platform.OS == "web" ? (localStorage.getItem("v2") ? true : false) : false
