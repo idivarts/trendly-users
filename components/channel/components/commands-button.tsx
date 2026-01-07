828bcee0c3aaee76c0b2d83036b2302a359f7b7c
Rahul Sinha <rahul@idiv.in>
2026-01-07 20:15:44 +0530

    Indentation fix

--- components/channel/components/commands-button.tsx
+++ components/channel/components/commands-button.tsx
@@ -6,107 +6,107 @@ import React from "react";
 import type { GestureResponderEvent } from "react-native";
 import { Pressable } from "react-native";
 import {
-  DefaultStreamChatGenerics,
-  isSuggestionCommand,
-  SuggestionsContextValue,
-  useSuggestionsContext,
-  useTheme,
+    DefaultStreamChatGenerics,
+    isSuggestionCommand,
+    SuggestionsContextValue,
+    useSuggestionsContext,
+    useTheme,
 } from "stream-chat-expo";
 
 type CommandsButtonPropsWithContext<
-  StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
+    StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
 > = Pick<SuggestionsContextValue<StreamChatGenerics>, "suggestions"> & {
-  /** Function that opens commands selector */
-  handleOnPress?: ((event: GestureResponderEvent) => void) & (() => void);
+    /** Function that opens commands selector */
+    handleOnPress?: ((event: GestureResponderEvent) => void) & (() => void);
 };
 
 const CommandsButtonWithContext = <
-  StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
+    StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
 >(
-  props: CommandsButtonPropsWithContext<StreamChatGenerics>
+    props: CommandsButtonPropsWithContext<StreamChatGenerics>
 ) => {
-  const { handleOnPress, suggestions } = props;
+    const { handleOnPress, suggestions } = props;
 
-  const {
-    theme: {
-      colors: { accent_blue, grey },
-      messageInput: { commandsButton },
-    },
-  } = useTheme();
-  const appTheme = useAppTheme();
+    const {
+        theme: {
+            colors: { accent_blue, grey },
+            messageInput: { commandsButton },
+        },
+    } = useTheme();
+    const appTheme = useAppTheme();
 
-  return (
-    <Pressable
-      onPress={handleOnPress}
-      style={[commandsButton]}
-      testID="commands-button"
-    >
-      <FontAwesomeIcon
-        color={
-          suggestions &&
-          suggestions.data.some((suggestion) => isSuggestionCommand(suggestion))
-            ? Colors(appTheme).primary
-            : Colors(appTheme).primary
-        }
-        icon={faBolt}
-        size={22}
-        style={{
-          marginTop: 4,
-        }}
-      />
-    </Pressable>
-  );
+    return (
+        <Pressable
+            onPress={handleOnPress}
+            style={[commandsButton]}
+            testID="commands-button"
+        >
+            <FontAwesomeIcon
+                color={
+                    suggestions &&
+                        suggestions.data.some((suggestion) => isSuggestionCommand(suggestion))
+                        ? Colors(appTheme).primary
+                        : Colors(appTheme).primary
+                }
+                icon={faBolt}
+                size={22}
+                style={{
+                    marginTop: 4,
+                }}
+            />
+        </Pressable>
+    );
 };
 
 const areEqual = <
-  StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
+    StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
 >(
-  prevProps: CommandsButtonPropsWithContext<StreamChatGenerics>,
-  nextProps: CommandsButtonPropsWithContext<StreamChatGenerics>
+    prevProps: CommandsButtonPropsWithContext<StreamChatGenerics>,
+    nextProps: CommandsButtonPropsWithContext<StreamChatGenerics>
 ) => {
-  const { suggestions: prevSuggestions } = prevProps;
-  const { suggestions: nextSuggestions } = nextProps;
+    const { suggestions: prevSuggestions } = prevProps;
+    const { suggestions: nextSuggestions } = nextProps;
 
-  const suggestionsEqual = !!prevSuggestions === !!nextSuggestions;
-  if (!suggestionsEqual) return false;
+    const suggestionsEqual = !!prevSuggestions === !!nextSuggestions;
+    if (!suggestionsEqual) return false;
 
-  return true;
+    return true;
 };
 
 const MemoizedCommandsButton = React.memo(
-  CommandsButtonWithContext,
-  areEqual
+    CommandsButtonWithContext,
+    areEqual
 ) as typeof CommandsButtonWithContext;
 
 export type CommandsButtonProps<
-  StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
+    StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
 > = Partial<CommandsButtonPropsWithContext<StreamChatGenerics>>;
 
 /**
  * UI Component for attach button in MessageInput component.
  */
 export const CommandsButton = <
-  StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
+    StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
 >(
-  props: CommandsButtonProps<StreamChatGenerics>
+    props: CommandsButtonProps<StreamChatGenerics>
 ) => {
-  let suggestions:
-    | CommandsButtonPropsWithContext<StreamChatGenerics>["suggestions"]
-    | undefined;
+    let suggestions:
+        | CommandsButtonPropsWithContext<StreamChatGenerics>["suggestions"]
+        | undefined;
 
-  if (typeof useSuggestionsContext === "function") {
-    try {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const ctx = (useSuggestionsContext as any)<StreamChatGenerics>();
-      suggestions = ctx?.suggestions;
-    } catch (e) {
-      suggestions = undefined;
+    if (typeof useSuggestionsContext === "function") {
+        try {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const ctx = (useSuggestionsContext as any)<StreamChatGenerics>();
+            suggestions = ctx?.suggestions;
+        } catch (e) {
+            suggestions = undefined;
+        }
+    } else {
+        suggestions = undefined;
     }
-  } else {
-    suggestions = undefined;
-  }
 
-  return <MemoizedCommandsButton {...{ suggestions }} {...props} />;
+    return <MemoizedCommandsButton {...{ suggestions }} {...props} />;
 };
 
 CommandsButton.displayName = "CommandsButton{messageInput}";
