e389404f936b13f8e37fb76a483cf195e696170c
jerry-xon <jppurswani2004@gmail.com>
2026-04-22 21:13:21 +0530

    bug fix

--- components/collaboration/collaboration-details/CollaborationDetailsContent.tsx
+++ components/collaboration/collaboration-details/CollaborationDetailsContent.tsx
@@ -214,6 +214,19 @@ const CollaborationDetailsContent = (
     useEffect(() => {
         fetchContracts();
     }, []);
+
+    const applicationContract = React.useMemo(() => {
+        const applicationId = props.applicationData?.id;
+        if (!applicationId) return undefined;
+
+        return contracts.find((contract) => {
+            const anyContract = contract as any;
+            return (
+                anyContract.applicationId === applicationId ||
+                contract.userId === applicationId
+            );
+        });
+    }, [contracts, props.applicationData?.id]);
     return (
         <ScrollView
             contentContainerStyle={styles.scrollContainer}
@@ -310,8 +323,15 @@ const CollaborationDetailsContent = (
                                 props?.collaborationDetail?.questionsToInfluencers
                             }
                             onWithdrawPress={() => setConfirmationModalVisible(true)}
+                            editLabel={applicationContract ? "View Contract" : "Edit"}
                             onEditPress={() => {
                                 if (!props.applicationData?.id) return;
+
+                                if (applicationContract?.id) {
+                                    router.push(`/contract-details/${applicationContract.id}`);
+                                    return;
+                                }
+
                                 router.push({
                                     // @ts-ignore
                                     pathname: `/edit-application/${props.applicationData.id}`,
